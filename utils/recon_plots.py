7ced0f1f1dcf8f5dee56ef4bb26406755cb4650b
ngu183 <ngu183@csiro.au>
2023-01-19 12:32:25 +1100

    F

--- utils/recon_plots.py
+++ utils/recon_plots.py
@@ -2,13 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import os
+import random
 
 from .constant import CHART_TO_HEAD_MAP, UNIQ_CHART_HEADS
+
+from matplotlib.colors import ListedColormap
 
 SINGLE_PLOT_TIGHT_LAYOUT_REC = [0, 0.2, 1.0, 1.0]
 SINGLE_PLOT_TEXT_LOC = [0.5, 0.05]
 MAX_SENT_CAPTION = 2
 MAX_TEXT_CAPTION = 400
+
+COLOR_PALETTES = [
+    ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6'],
+    ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'],
+    ['#8dd3c7','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9'],
+    ['#d0d1e6','#a6bddb','#74a9cf','#3690c0','#0570b0','#045a8d','#023858'],
+    ['#ccece6','#99d8c9','#66c2a4','#41ae76','#238b45','#006d2c','#00441b'],
+    ['#d4b9da','#c994c7','#df65b0','#e7298a','#ce1256','#980043','#67001f']
+]
 
 def unscale_conts(cont_preds, scale_preds, tab_shape=None, scale_eps=1.00001, scale_exponent=10):
     if tab_shape is not None:
@@ -156,15 +168,15 @@ def prepare_mpl(x):
         chart_list.append(chart_dict)
     return chart_list
 
-def create_bar_chart(x_data, text, f_name, xhat_data=None, bar_width=0.75):
+def create_bar_chart(x_data, text, f_name, xhat_data=None, bar_width=0.75, step=None):
 
     if xhat_data is not None:
-        _create_bar_chart_double(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name, bar_width=bar_width)
+        _create_bar_chart_double(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name, bar_width=bar_width, step=step)
     else:
-        _create_bar_chart_single(x_data=x_data, text=text, f_name=f_name, base_width=bar_width)
+        _create_bar_chart_single(x_data=x_data, text=text, f_name=f_name, base_width=bar_width, step=step)
 
 
-def _create_bar_chart_single(x_data, text, f_name, base_width=0.25, flip_thres=4):
+def _create_bar_chart_single(x_data, text, f_name, base_width=0.25, flip_thres=4, step=None):
     recon_values = x_data['values']
     
     _ = plt.figure()
@@ -178,6 +190,12 @@ def _create_bar_chart_single(x_data, text, f_name, base_width=0.25, flip_thres=4
     #clip by number of available categorical values
     max_cat = min(len(text['categorical']), len(recon_values[0]))
     max_series = min(len(text['series_name']), len(recon_values))
+
+    if step is None:
+        cidx = random.choice(range(len(COLOR_PALETTES)))
+        cmap = COLOR_PALETTES[cidx]
+    else:
+        cmap = COLOR_PALETTES[step % len(COLOR_PALETTES)]
 
     #Manually remove
     if max_cat <= 1 or max_series <= 1:
@@ -196,6 +214,8 @@ def _create_bar_chart_single(x_data, text, f_name, base_width=0.25, flip_thres=4
     for idx, (y, label)in enumerate(zip(recon_values, text['series_name'])):
         if idx > max_series:
             break
+        
+        color = cmap[idx % len(cmap)]
 
         y = y[:max_cat]
         if idx == 0:
@@ -212,9 +232,9 @@ def _create_bar_chart_single(x_data, text, f_name, base_width=0.25, flip_thres=4
                 max_label_len = len(label)
         
         if max_cat > flip_thres:
-            plt.barh(br1, y, height=bar_width, label=label)
+            plt.barh(br1, y, height=bar_width, label=label, color=color)
         else:
-            plt.bar(br1, y, width=bar_width, label=label)
+            plt.bar(br1, y, width=bar_width, label=label, color=color)
 
         #plt.bar(br1, y, width=bar_width, label=label)
     
@@ -275,7 +295,7 @@ def _create_bar_chart_single(x_data, text, f_name, base_width=0.25, flip_thres=4
     plt.savefig(fname=f_name)
     plt.close()
 
-def _create_bar_chart_double(x_data, xhat_data, text, f_name, bar_width=0.25):
+def _create_bar_chart_double(x_data, xhat_data, text, f_name, bar_width=0.25, step=None):
 
     # Create original plot
     original_values = x_data['values']
@@ -284,6 +304,11 @@ def _create_bar_chart_double(x_data, xhat_data, text, f_name, bar_width=0.25):
     #Pick horizontal or horizontal based on xticks
 
     #plt.title('Original')
+    if step is None:
+        cidx = random.choice(range(len(COLOR_PALETTES)))
+        cmap = COLOR_PALETTES[cidx]
+    else:
+        cmap = COLOR_PALETTES[step % len(COLOR_PALETTES)]
 
     #plt.xlabel('x-axis')
     if len(text['axis_titles']) > 0:
@@ -301,6 +326,8 @@ def _create_bar_chart_double(x_data, xhat_data, text, f_name, bar_width=0.25):
         ticks = [r + bar_width for r in range(len(original_values[0]))]
         
     for idx, y in enumerate(original_values):
+        color = cmap[idx % len(cmap)]
+
         if idx == 0:
             br1 = np.arange(len(y))
         else:
@@ -311,9 +338,9 @@ def _create_bar_chart_double(x_data, xhat_data, text, f_name, bar_width=0.25):
             label = None
         
         if len(text['categorical']) > 5:
-            plt.barh(br1, y, height=bar_width, label=label)
+            plt.barh(br1, y, height=bar_width, label=label, color=color)
         else:
-            plt.bar(br1, y, width=bar_width, label=label)
+            plt.bar(br1, y, width=bar_width, label=label, color=color)
 
     rotation = 0
     wrap = True
@@ -350,14 +377,8 @@ def _create_bar_chart_double(x_data, xhat_data, text, f_name, bar_width=0.25):
     #clip by number of available categorical values
     clip_val = min(len(text['categorical']), len(recon_values[0]))
 
-    if len(recon_values) == 1:
-        bar_width = 0.75
-        ticks = [r for r in range(len(recon_values[0]))]
-    else:
-        bar_width = 0.75 / clip_val
-        ticks = [r + bar_width for r in range(len(recon_values[0]))]
-
     for idx, (y, label)in enumerate(zip(recon_values, text['series_name'])):
+        color = cmap[idx % len(cmap)]
 
         #label = [l for l in label if 'unnamed' not in l]
         if 'unnamed' in label:
@@ -370,9 +391,9 @@ def _create_bar_chart_double(x_data, xhat_data, text, f_name, bar_width=0.25):
             br1 = [x + bar_width for x in br1][:clip_val]
 
         if len(text['categorical']) > 5:
-            plt.barh(br1, y, height=bar_width, label=label)
+            plt.barh(br1, y, height=bar_width, label=label, color=color)
         else:
-            plt.bar(br1, y, width=bar_width, label=label)
+            plt.bar(br1, y, width=bar_width, label=label, color=color)
 
     rotation = 0
     wrap = True
@@ -388,9 +409,6 @@ def _create_bar_chart_double(x_data, xhat_data, text, f_name, bar_width=0.25):
         plt.xticks(ticks[:clip_val], text['categorical'][:clip_val], wrap=wrap, fontsize=fontsize, rotation=rotation)
 
     if len(recon_values) > 1:
-        # if len(recon_values) <= 2:
-        #     plt.legend()
-        # else:
         plt.legend(bbox_to_anchor=(1,1), loc="upper left")
 
     #Add caption
@@ -406,19 +424,24 @@ def _create_bar_chart_double(x_data, xhat_data, text, f_name, bar_width=0.25):
     plt.savefig(fname=f_name)
     plt.close()
 
-def create_scatter(x_data, text, f_name, xhat_data=None, bar_width=0.25):
+def create_scatter(x_data, text, f_name, xhat_data=None, bar_width=0.25, step=None):
 
     if xhat_data is not None:
-        _create_scatter_double(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name)
+        _create_scatter_double(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name, step=step)
     else:
-        _create_scatter_single(x_data=x_data, text=text, f_name=f_name)
+        _create_scatter_single(x_data=x_data, text=text, f_name=f_name, step=step)
 
-
-def _create_scatter_single(x_data, text, f_name):
+def _create_scatter_single(x_data, text, f_name, markersize=4, step=None):
     x_values = x_data['values']['x']
     y_values = x_data['values']['y']
     fig = plt.figure()
     plt.figure(facecolor='white')
+
+    if step is None:
+        cidx = random.choice(range(len(COLOR_PALETTES)))
+        cmap = ListedColormap(COLOR_PALETTES[cidx])
+    else:
+        cmap = ListedColormap(COLOR_PALETTES[step % len(COLOR_PALETTES)])
 
     #Remove repeats between categorical and series names
     text['series_name'] = [s for s in text['series_name'] if 'unnamed'  not in s]
@@ -437,9 +460,7 @@ def _create_scatter_single(x_data, text, f_name):
             if len(label) > max_label_len:
                 max_label_len = len(label)
 
-        plt.scatter(x, y, cmap = 'viridis', label=label)
-
-
+        plt.scatter(x, y, cmap=cmap, label=label, s=markersize)
 
     if len(x_values) > 1 and label_count > 0:
         if max_label_len <= 5:
@@ -480,7 +501,13 @@ def _create_scatter_single(x_data, text, f_name):
     plt.close()
 
 
-def _create_scatter_double(x_data, xhat_data, text, f_name):
+def _create_scatter_double(x_data, xhat_data, text, f_name, step=None):
+
+    if step is None:
+        cidx = random.choice(range(len(COLOR_PALETTES)))
+        cmap = ListedColormap(COLOR_PALETTES[cidx])
+    else:
+        cmap = ListedColormap(COLOR_PALETTES[step % len(COLOR_PALETTES)])
 
     x_values = x_data['values']['x']
     y_values = x_data['values']['y']
@@ -496,7 +523,7 @@ def _create_scatter_double(x_data, xhat_data, text, f_name):
     for idx, (x,y, label) in enumerate(zip(x_values, y_values, text['series_name'])):
         if 'unnamed' in label:
             label = None
-        plt.scatter(x, y, cmap = 'viridis', label=label)
+        plt.scatter(x, y, cmap=cmap, label=label)
 
     if len(x_values) > 1:
         plt.legend(bbox_to_anchor=(1,1), loc="upper left")
@@ -516,7 +543,7 @@ def _create_scatter_double(x_data, xhat_data, text, f_name):
     for idx, (x,y, label) in enumerate(zip(x_values, y_values, text['series_name'])):
         if 'unnamed' in label:
             label = None
-        plt.scatter(x, y, cmap = 'viridis', label=label)
+        plt.scatter(x, y, cmap=cmap, label=label)
 
     if len(x_values) > 1:
         plt.legend(bbox_to_anchor=(1,1), loc="upper left")
@@ -534,7 +561,7 @@ def _create_scatter_double(x_data, xhat_data, text, f_name):
     plt.savefig(fname=f_name)
     plt.close()
 
-def create_boxplot(x_data, text, f_name, xhat_data=None):
+def create_boxplot(x_data, text, f_name, xhat_data=None, step=None):
 
     if xhat_data is not None:
         create_boxplot_double(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name)
@@ -629,12 +656,227 @@ def create_recon_plots(x, x_hat, text_data, step, epoch_dir):
 
         f_name = os.path.join(epoch_dir, f"{step}-{idx}-{xhat_data['head_type']}.png")
         if x_data['head_type'] == 'categorical':
-            create_bar_chart(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name)
+            create_bar_chart(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name, step=step)
         elif x_data['head_type'] == 'point':
-            create_scatter(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name)
+            create_line(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name, step=step)
         else:
-            create_boxplot(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name)
+            create_boxplot(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name, step=step)
+
+
+def create_line(x_data, text, f_name, xhat_data=None, bar_width=0.25, markersize=3, step=None):
+
+    if xhat_data is not None:
+        _create_line_double(x_data=x_data, xhat_data=xhat_data, text=text, f_name=f_name, markersize=markersize, step=step)
+    else:
+        _create_line_single(x_data=x_data, text=text, f_name=f_name, markersize=markersize, step=step)
+
+def _create_line_single(x_data, text, f_name, markersize=4, linewidth=2, step=None):
+
+    x_values = x_data['values']['x']
+    y_values = x_data['values']['y']
+    fig, ax = plt.subplots()
+
+    plt.figure(facecolor='white')
+    
+    if step is None:
+        cidx = random.choice(range(len(COLOR_PALETTES)))
+        cmap = COLOR_PALETTES[cidx]
+    else:
+        cmap = COLOR_PALETTES[step % len(COLOR_PALETTES)]
+
+    #Remove repeats between categorical and series names
+    text['series_name'] = [s for s in text['series_name'] if 'unnamed'  not in s]
+
+    label_count = 0
+    max_label_len = 0
+    #print(f_name, len(x_values[0]), len(x_values), len(y_values[0]), len(y_values))
+    if len(x_values[0]) == 1:
+        return
+
+    for idx, (x, y, label) in enumerate(zip(x_values, y_values, text['series_name'])):
+        if 'unnamed' in label:
+            label = None
+        else:
+            label_count += 1
+            if len(label) > max_label_len:
+                max_label_len = len(label)
+
+        x, y = zip(*sorted(zip(x, y)))
+
+        color = cmap[idx % len(cmap)]
+        plt.plot(x, y, label=label, 
+            markersize=markersize,  color=color, 
+            marker='o', linewidth=linewidth)
+    
+    if len(x_values) > 1 and label_count > 0:
+        if max_label_len <= 5:
+            #Short legend box on the right
+            plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+        elif max_label_len >= 40:
+            #Long legend box should be put on the top
+            plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", mode="expand", ncol=1)
+
+        elif label_count >= 4:
+            #Lots of labels, put on the right
+            plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+
+        else:
+            if max_label_len > 20:
+                ncol = 1
+            else:
+                ncol = label_count
             
+            plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", mode="expand", ncol=ncol)
+
+    if len(text['axis_titles']) > 0:
+        axis_title1 = text['axis_titles'][0].split('.')[0]
+        plt.xlabel(axis_title1)
+    if len(text['axis_titles']) > 1:
+        axis_title2 = text['axis_titles'][1].split('.')[0]
+        plt.ylabel(axis_title2)
+
+    #Only keep first 3 sentences
+    caption = text['caption'].replace('\n','')
+    caption = '.'.join(caption.split('.')[:MAX_SENT_CAPTION])[:MAX_TEXT_CAPTION]
+
+    s = SINGLE_PLOT_TEXT_LOC
+    plt.figtext(s[0], s[1], caption, wrap=True, horizontalalignment='center')
+
+    plt.tight_layout(rect=SINGLE_PLOT_TIGHT_LAYOUT_REC)
+    plt.savefig(fname=f_name)
+    plt.close()
+
+
+def _create_line_double(x_data, xhat_data, text, f_name,  markersize=4, linewidth=2, step=None):
+
+    x_values = x_data['values']['x']
+    y_values = x_data['values']['y']
+    fig, ax = plt.subplots()
+
+    plt.subplot(2, 1, 1)  
+
+    if step is None:
+        cidx = random.choice(range(len(COLOR_PALETTES)))
+        cmap = COLOR_PALETTES[cidx]
+    else:
+        cmap = COLOR_PALETTES[step % len(COLOR_PALETTES)]
+
+    text['series_name'] = [s for s in text['series_name'] if 'unnamed'  not in s]
+
+    ## start from here...
+    label_count = 0
+    max_label_len = 0
+    if len(x_values[0]) == 1:
+        return
+
+    if len(text['axis_titles']) > 0:
+        axis_title1 = text['axis_titles'][0].split('.')[0]
+        plt.xlabel(axis_title1)
+    if len(text['axis_titles']) > 1:
+        axis_title2 = text['axis_titles'][1].split('.')[0]
+        plt.ylabel(axis_title2)
+
+    for idx, (x,y, label) in enumerate(zip(x_values, y_values, text['series_name'])):
+        if 'unnamed' in label:
+            label = None
+        else:
+            label_count += 1
+            if len(label) > max_label_len:
+                max_label_len = len(label)
+        
+        x, y = zip(*sorted(zip(x, y)))
+        color = cmap[idx % len(cmap)]
+
+        plt.plot(x, y, label=label,
+            markersize=markersize,  color=color, 
+            marker='o', linewidth=linewidth)
+
+    if len(x_values) > 1 and label_count > 0:
+        if max_label_len <= 5:
+            #Short legend box on the right
+            plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+        elif max_label_len >= 40:
+            #Long legend box should be put on the top
+            plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", mode="expand", ncol=1)
+
+        elif label_count >= 4:
+            #Lots of labels, put on the right
+            plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+
+        else:
+            if max_label_len > 20:
+                ncol = 1
+            else:
+                ncol = label_count
+            
+            plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", mode="expand", ncol=ncol)
+
+    if len(text['axis_titles']) > 0:
+        axis_title1 = text['axis_titles'][0].split('.')[0]
+        plt.xlabel(axis_title1)
+    if len(text['axis_titles']) > 1:
+        axis_title2 = text['axis_titles'][1].split('.')[0]
+        plt.ylabel(axis_title2)
+
+    # Reconstruction
+    x_values = xhat_data['values']['x']
+    y_values = xhat_data['values']['y']
+
+    plt.subplot(2, 1, 2)
+    #plt.title('Reconstruction')
+    if len(text['axis_titles']) > 0:
+        axis_title1 = text['axis_titles'][0].split('.')[0]
+        plt.xlabel(axis_title1)
+    if len(text['axis_titles']) > 1:
+        axis_title2 = text['axis_titles'][1].split('.')[0]
+        plt.ylabel(axis_title2)
+    
+    for idx, (x,y, label) in enumerate(zip(x_values, y_values, text['series_name'])):
+        if 'unnamed' in label:
+            label = None
+
+        x, y = zip(*sorted(zip(x, y)))
+        color = cmap[idx % len(cmap)]
+
+        plt.plot(x, y, label=label,
+            markersize=markersize,  color=color, 
+            marker='o', linewidth=linewidth)
+
+    label_count = 0
+    max_label_len = 0
+    if len(x_values) > 1 and label_count > 0:
+        if max_label_len <= 5:
+            #Short legend box on the right
+            plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+        elif max_label_len >= 40:
+            #Long legend box should be put on the top
+            plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", mode="expand", ncol=1)
+
+        elif label_count >= 4:
+            #Lots of labels, put on the right
+            plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+
+        else:
+            if max_label_len > 20:
+                ncol = 1
+            else:
+                ncol = label_count
+            
+            plt.legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", mode="expand", ncol=ncol)
+
+    #Add caption
+    #plt.subplots_adjust(bottom=0.2) 
+    plt.tight_layout(rect=[0, 0.0, 1.0, 0.8])
+
+    #Only keep first 3 sentences
+    caption = text['caption'].replace('\n','')
+    caption = '.'.join(caption.split('.')[:MAX_SENT_CAPTION])[:MAX_TEXT_CAPTION]
+    plt.figtext(0.5, 0.9, caption, wrap=True, horizontalalignment='center')
+
+    # space between the plots
+    plt.savefig(fname=f_name)
+    plt.close()
+
 
 def create_single_plot(x, text_data, save_dir, step):
 
@@ -649,11 +891,11 @@ def create_single_plot(x, text_data, save_dir, step):
       text = text_data[idx]
 
       if x_data['head_type'] == 'categorical':
-          create_bar_chart(x_data, text, f_name)
+          create_bar_chart(x_data, text, f_name, step=step)
       elif x_data['head_type'] == 'point':
-          create_scatter(x_data, text, f_name)
+          create_line(x_data, text, f_name, step=step)
       elif x_data['head_type'] == 'boxplot':
-          create_boxplot(x_data, text, f_name)
+          create_boxplot(x_data, text, f_name, step=step)
       else:
         raise ValueError(f"Invalid chart type given: {x_data['head_type']}")
       plt.close()
